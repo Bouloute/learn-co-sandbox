bc5a07bfe019ab6730329a636434b393b0ed276a
Anna Lewis Dagnino <annaick1995@gmail.com>
2021-02-18 14:53:57 +0000

    Automatically backed up by Learn

---
+++ testing.rb
@@ -0,0 +1,17 @@
+class Album
+  @@album_count = 0 
+ 
+  def initialize
+    @@album_count += 1
+  end
+ 
+  def self.count
+     puts @@album_count
+  end
+end
+
+Album.new
+Album.new
+Album.new
+ 
+Album.count
